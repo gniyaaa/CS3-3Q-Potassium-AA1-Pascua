bef704c2d9aa81312813123545b338e7335edfb4
gniyaaa <lmmpascua22@src.pshs.edu.ph>
2025-01-22 20:39:12 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const pi = 3.14;
+
+let radius = 6.378e6;;
+document.write("The radius of the sphere is " + radius + "." + "<br>");
+
+let circumference = 2 * 3.14 * radius;
+document.write("The circumference of the sphere is " + circumference + "." + "<br>");
+
+let surfaceArea = 4 * 3.14 * (radius ** 2);
+document.write("The surface area of the sphere is " + surfaceArea + "." + "<br>");
+
+let volumeOfSphere = (4/3) * pi * (radius ** 3)
+document.write("The volume of the sphere is " + volumeOfSphere + "." + "<br>");
+
+
+
+
+
+
